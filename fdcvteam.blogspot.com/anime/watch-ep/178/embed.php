22eb0d3dbfe8b038c6c391f0d8ca0c3cc702d56e
hunganhfb <75318518+hunganhfb@users.noreply.github.com>
2021-12-21 09:32:41 +0700

    Create embed.php

---
+++ fdcvteam.blogspot.com/anime/watch-ep/178/embed.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>"Fan Detective Conan Vietnamese - FDCV: EP1031 "                                                   </title>
+    <!-- Required meta tags -->
+    <link rel="icon" href="https://www.google.com/s2/favicons?domain=fdcv.xyz">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="https://raw.githubusercontent.com/admin1509/admin1509/main/getvid/lotus/jwplayer.js" type="c7b14115fae32e32d772e4b1-text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" type="c7b14115fae32e32d772e4b1-text/javascript"></script>
+</head>
+
+<body>
+    <center>
+        <div id="mediaplayer"></div>
+        <script type="c7b14115fae32e32d772e4b1-text/javascript">
+            var playerInstance = jwplayer("mediaplayer").setup({
+                "file": "https://raw.githubusercontent.com/admin1509/admin1509/main/fdcv.xyz/watch-ep/178/index.m3u8",
+                "type": "mp4",
+                "image": "https://user-images.githubusercontent.com/75318518/146860692-12075259-7e81-4e93-baa1-b25f14df7ef3.png",
+                "height": "100%",
+                "width": "100%",
+                abouttext: 'FDCV',
+                aboutlink: 'https://www.facebook.com/conanfdcv'
+            });
+            playerInstance.on('error', function() {
+            playerInstance.load({
+                file: "//content.jwplatform.com/videos/7RtXk3vl-52qL9xLP.mp4",
+                image: "//content.jwplatform.com/thumbs/7RtXk3vl-480.jpg"
+            });
+            playerInstance.play()
+        });
+            </script>
+    </center>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous" type="c7b14115fae32e32d772e4b1-text/javascript"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous" type="c7b14115fae32e32d772e4b1-text/javascript"></script>
+<script src="https://raw.githubusercontent.com/admin1509/admin1509/main/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="c7b14115fae32e32d772e4b1-|49" defer=""></script></body>
+
+</html>
